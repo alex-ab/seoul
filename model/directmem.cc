09546585575b1dc0af30da0238fc818810c44235
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2023-10-23 20:07:54 +0200

    Add readonly support for directmem

--- model/directmem.cc
+++ model/directmem.cc
@@ -26,9 +26,10 @@
  */
 class DirectMemDevice : public StaticReceiver<DirectMemDevice>
 {
-  char *_ptr;
-  uintptr_t _phys;
-  size_t _size;
+  char      * const _ptr;
+  uintptr_t   const _phys;
+  size_t      const _size;
+  bool        const _readonly;
 
   /*
    * Noncopyable
@@ -37,55 +38,65 @@ class DirectMemDevice : public StaticReceiver<DirectMemDevice>
   DirectMemDevice &operator = (DirectMemDevice const &);
 
  public:
+
   bool  receive(MessageMemRegion &msg)
   {
-    if (!in_range(msg.page, _phys >> 12, _size >> 12))  return false;
+    if (!in_range(msg.page, _phys >> 12, _size >> 12))
+      return false;
+
     Logging::printf("%s: %p base %lx+%zx\n", __PRETTY_FUNCTION__, _ptr, _phys, _size);
+
     msg.start_page = _phys >> 12;
-    msg.count = unsigned(_size >> 12);
-    msg.ptr   = _ptr;
+    msg.count      = unsigned(_size >> 12);
+    msg.ptr        = _ptr;
+    msg.read_only  = _readonly;
     return true;
   }
 
 
-  bool  receive(MessageMem &msg)
+  bool receive(MessageMem &msg) const
   {
-    unsigned *ptr;
-    if (in_range(msg.phys, _phys, _size))
-      ptr = reinterpret_cast<unsigned *>(_ptr + msg.phys - _phys);
-    else return false;
+    if (!in_range(msg.phys, _phys, _size))
+      return false;
+
+    if (_readonly && !msg.read)
+      return false;
+
+    unsigned * ptr = reinterpret_cast<unsigned *>(_ptr + msg.phys - _phys);
 
     if (msg.read) *msg.ptr = *ptr; else *ptr = *msg.ptr;
+
     return true;
   }
 
 
-  DirectMemDevice(char *ptr, uintptr_t phys, size_t size) : _ptr(ptr), _phys(phys), _size(size)
+  DirectMemDevice(char *ptr, uintptr_t phys, size_t size, bool readonly)
+  : _ptr(ptr), _phys(phys), _size(size), _readonly(readonly)
   {
-    Logging::printf("DirectMem: %p base %lx+%zx\n", ptr, phys, size);
+    Logging::printf("directmem: %p base %lx+%zx %s\n",
+                    ptr, phys, size, _readonly ? "readonly" : "");
   }
 };
 
 
 PARAM_HANDLER(mio,
-	      "mio:base,size,dest=base - map hostmemory directly into the VM.",
+	      "mio:base,size,readonly,dest=base - map hostmemory directly into the VM.",
 	      "Example: 'mio:0xa0000,0x10000'.")
 {
-  size_t size;
-  uintptr_t dest = (argv[2] == ~0UL) ? argv[0] : argv[2];
-  if ( argv[1] == ~0UL)
-    size = 1;
-  else
-    size = Cpu::bsr(argv[1] | 1);
+  auto const invalid_arg = ~0UL;
+  auto const guest_base  = argv[0];
+  bool const readonly    = !!argv[2];
+  auto const dest        = (argv[3] == invalid_arg) ? guest_base : argv[3];
 
+  if (argv[0] == invalid_arg || argv[1] == invalid_arg || argv[2] == invalid_arg)
+    Logging::panic("directmem - invalid parameters\n");
 
-  MessageHostOp msg(MessageHostOp::OP_ALLOC_IOMEM, argv[0], 1UL << size);
+  MessageHostOp msg(MessageHostOp::OP_ALLOC_IOMEM, guest_base, argv[1]);
   if (!mb.bus_hostop.send(msg) || !msg.ptr)
     Logging::panic("can not map IOMEM region %lx+%zx", msg.value, msg.len);
 
-  DirectMemDevice *dev = new DirectMemDevice(msg.ptr, dest, 1UL << size);
-  mb.bus_memregion.add(dev,  DirectMemDevice::receive_static<MessageMemRegion>);
-  mb.bus_mem.add(dev,        DirectMemDevice::receive_static<MessageMem>);
+  DirectMemDevice *dev = new DirectMemDevice(msg.ptr, dest, argv[1], readonly);
 
+  mb.bus_memregion.add(dev, DirectMemDevice::receive_static<MessageMemRegion>);
+  mb.bus_mem      .add(dev, DirectMemDevice::receive_static<MessageMem>);
 }
-
