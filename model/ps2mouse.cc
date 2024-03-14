eee968fceb21e2a38cbc950a6cf7961fd202d231
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2024-03-14 18:04:15 +0100

    model/ps2mouse: add explicit synchronization

--- model/ps2mouse.cc
+++ model/ps2mouse.cc
@@ -17,6 +17,7 @@
  */
 
 #include "nul/motherboard.h"
+#include "service/lock.h"
 
 /**
  * A PS2 keyboard gets mouse packets the hostbus as input and outputs
@@ -29,8 +30,11 @@
  */
 class PS2Mouse : public StaticReceiver<PS2Mouse>
 {
-  unsigned static const HOST_RESOLUTION_SHIFT = 2;
-  DBus<MessagePS2> &_bus_ps2;
+	unsigned static const HOST_RESOLUTION_SHIFT = 2;
+
+	DBus<MessagePS2> &_bus_ps2;
+	Seoul::Lock       _lock { };
+
   unsigned _ps2port;
   unsigned _hostmouse;
   unsigned long long _packet { 0 };
@@ -133,175 +137,182 @@ class PS2Mouse : public StaticReceiver<PS2Mouse>
     return value;
   }
 
-	void update_packet()
+ public:
+
+	bool receive(MessageInput &msg)
 	{
-		if (!(_packet & 0xff) && ~_status & STATUS_REMOTE) {
-			_packet = gen_packet(true);
+		if (msg.device != _hostmouse)
+			return false;
+
+		bool notify = false;
+
+		{
+			Seoul::Lock::Guard guard(_lock);
+
+			// we support only 3 byte packets
+			assert((msg.data & 0xff) == 3);
+
+			// we ignore the overflow bit as everybody does
+			_posx += ((msg.data >> 16) & 0xff) - ((msg.data >> 4) & 0x100);
+			_posy += ((msg.data >> 24) & 0xff) - ((msg.data >> 5) & 0x100);
+			_posz  = -msg.data2 & 0xff;
+			_status = uint8((_status & 0xf8) | ((msg.data >> 8) & 0x7));
+
+			if (!(_packet & 0xff) && ~_status & STATUS_REMOTE) {
+				_packet = gen_packet(true);
+				notify = true;
+			}
+		}
+
+		if (notify) {
 			MessagePS2 msg2(_ps2port, MessagePS2::NOTIFY, 0);
 			_bus_ps2.send(msg2);
 		}
+
+		return true;
 	}
 
- public:
-  bool  receive(MessageInput &msg)
-  {
-    if (msg.device != _hostmouse)
-      return false;
 
-    // we support only 3 byte packets
-    assert((msg.data & 0xff) == 3);
+	bool receive(MessagePS2 &msg)
+	{
+		if (msg.port != _ps2port)
+			return false;
 
-    // we ignore the overflow bit as everybody does
-    _posx += ((msg.data >> 16) & 0xff) - ((msg.data >> 4) & 0x100);
-    _posy += ((msg.data >> 24) & 0xff) - ((msg.data >> 5) & 0x100);
-    _posz  = -msg.data2 & 0xff;
-    _status = uint8((_status & 0xf8) | ((msg.data >> 8) & 0x7));
-    update_packet();
+		Seoul::Lock::Guard guard(_lock);
 
-    return true;
-  }
+		bool res = false;
+		if (msg.type == MessagePS2::READ_KEY) {
+			switch (_packet & 0xff) {
+			case 1 ... 4:
+				msg.value = (_packet >> 8) & 0xff;
+				_packet = ((_packet >> 8) & ~0xffULL)  | ((_packet & 0xff) - 1);
+				res = true;
+			default:
+				break;
+			}
+		} else if (msg.type == MessagePS2::SEND_COMMAND) {
+			res = true;
+			unsigned long long packet = 0;
+			switch (_param) {
+			case PARAM_SET_RESOLUTION:
+				_resolution = msg.value & 0x3;
+				_param = PARAM_NONE;
+				packet = 0xfa01;
+				break;
+			case PARAM_SET_SAMPLERATE:
+				// we do not check for magic sequences and odd values
+				_samplerate = msg.value;
+				_param = PARAM_NONE;
+				packet = 0xfa01;
+				break;
+			case PARAM_ECHO:
+				if (msg.value != 0xff && msg.value != 0xec) {
+					packet = msg.value << 8 | 1;
+					break;
+				}
 
+				[[fallthrough]];
 
-  bool  receive(MessagePS2 &msg)
-  {
-    if (msg.port != _ps2port)  return false;
-    bool res = false;
-    if (msg.type == MessagePS2::READ_KEY)
-      {
-	switch (_packet & 0xff)
-	  {
-	  case 1 ... 4:
-	    msg.value = (_packet >> 8) & 0xff;
-	    _packet = ((_packet >> 8) & ~0xffULL)  | ((_packet & 0xff) - 1);
-	    res = true;
-	  default:
-	    break;
-	  }
-      }
-    else if (msg.type == MessagePS2::SEND_COMMAND)
-      {
-	res = true;
-	unsigned long long packet = 0;
-	switch (_param)
-	  {
-	  case PARAM_SET_RESOLUTION:
-	    _resolution = msg.value & 0x3;
-	    _param = PARAM_NONE;
-	    packet = 0xfa01;
-	    break;
-	  case PARAM_SET_SAMPLERATE:
-	    // we do not check for magic sequences and odd values
-	    _samplerate = msg.value;
-	    _param = PARAM_NONE;
-	    packet = 0xfa01;
-	    break;
-	  case PARAM_ECHO:
-	    if (msg.value != 0xff && msg.value != 0xec)
-	      {
-		packet = msg.value << 8 | 1;
-		break;
-	      }
+			default:
+			case PARAM_NONE:
+				packet = 0xfa01;
+				switch (msg.value & 0xff) {
+				case 0xe6:  // set mouse scaling 1:1
+					_status &= uint8(~STATUS_SCALING);
+					break;
+				case 0xe7:  // set mouse scaling 1:2
+					_status |= STATUS_SCALING;
+					break;
+				case 0xe8: // set resolution
+					_param = PARAM_SET_RESOLUTION;
+					break;
+				case 0xe9: // status request
+					packet = union64(_samplerate, _resolution << 24 | _status << 16 | 0xfa00 | 4);
+					break;
+				case 0xea: // set stream mode
+					_status &= uint8(~STATUS_REMOTE);
+					break;
+				case 0xeb: // read packet
+					packet = gen_packet(false);
+					packet = (packet & ~0xff) << 16 | 0xfa00;
+					packet++;
+					break;
+				case 0xec: // clear echo mode
+					_param = PARAM_NONE;
+					break;
+				case 0xee: // echo mode
+					_param = PARAM_ECHO;
+					break;
+				case 0xf0: // set remote mode
+					_status |= STATUS_REMOTE;
+					break;
+				case 0xf2: // read id
+					packet = 0x00fa02 | (0x3 << 16);
+					break;
+				case 0xf3: // set sample rate
+					_param = PARAM_SET_SAMPLERATE;
+					break;
+				case 0xf4: // enable mouse
+					_status |= STATUS_ENABLED;
+					break;
+				case 0xf5: // disable mouse
+					_status &= uint8(~STATUS_ENABLED);
+					break;
+				case 0xf6: // set default
+					set_defaults();
+					packet = 0xfa01;
+					break;
+				case 0xff: // reset
+					set_defaults();
+					packet = 0x00aafa03;
+					break;
+				default:
+					Logging::printf("%s(%x, %x) unknown command\n",
+					                __PRETTY_FUNCTION__, msg.port, msg.value);
 
-	    [[fallthrough]];
+					[[fallthrough]];
 
-	  default:
-	  case PARAM_NONE:
-	    packet = 0xfa01;
-	    switch (msg.value & 0xff)
-	      {
-	      case 0xe6:  // set mouse scaling 1:1
-		_status &= uint8(~STATUS_SCALING);
-		break;
-	      case 0xe7:  // set mouse scaling 1:2
-		_status |= STATUS_SCALING;
-		break;
-	      case 0xe8: // set resolution
-		_param = PARAM_SET_RESOLUTION;
-		break;
-	      case 0xe9: // status request
-		packet = union64(_samplerate, _resolution << 24 | _status << 16 | 0xfa00 | 4);
-		break;
-	      case 0xea: // set stream mode
-		_status &= uint8(~STATUS_REMOTE);
-		break;
-	      case 0xeb: // read packet
-		packet = gen_packet(false);
-		packet = (packet & ~0xff) << 16 | 0xfa00;
-		packet++;
-		break;
-	      case 0xec: // clear echo mode
-		_param = PARAM_NONE;
-		break;
-	      case 0xee: // echo mode
-		_param = PARAM_ECHO;
-		break;
-	      case 0xf0: // set remote mode
-		_status |= STATUS_REMOTE;
-		break;
-	      case 0xf2: // read id
-		packet = 0x00fa02 | (0x3 << 16);
-		break;
-	      case 0xf3: // set sample rate
-		_param = PARAM_SET_SAMPLERATE;
-		break;
-	      case 0xf4: // enable mouse
-		_status |= STATUS_ENABLED;
-		break;
-	      case 0xf5: // disable mouse
-		_status &= uint8(~STATUS_ENABLED);
-		break;
-	      case 0xf6: // set default
-		set_defaults();
-		packet = 0xfa01;
-		break;
-	      case 0xff: // reset
-		set_defaults();
-		packet = 0x00aafa03;
-		break;
-	      default:
-		Logging::printf("%s(%x, %x) unknown command\n", __PRETTY_FUNCTION__, msg.port, msg.value);
+				case 0xe1: // read secondary ID - used to identify trackpoints
+					packet = 0xfc01;
+				}
+				break;
+			}
 
-		[[fallthrough]];
-
-	      case 0xe1: // read secondary ID - used to identify trackpoints
-		packet = 0xfc01;
-	      }
-	    break;
-	  }
-
-		if (packet & 0xff) {
-			_packet = packet;
-			msg.type = MessagePS2::NOTIFY_ON_REPLY;
+			if (packet & 0xff) {
+				_packet = packet;
+				msg.type = MessagePS2::NOTIFY_ON_REPLY;
+			}
 		}
-      }
-    return res;
-  }
+
+		return res;
+	}
 
 
-  void set_defaults()
-  {
-    _packet = 0;
-    _samplerate = 100;
-    _resolution = 2;
-    _status = 0x8;
-    _param = PARAM_NONE;
-    _posx = 0;
-    _posy = 0;
-  };
+	void set_defaults()
+	{
+		_packet = 0;
+		_samplerate = 100;
+		_resolution = 2;
+		_status = 0x8;
+		_param = PARAM_NONE;
+		_posx = 0;
+		_posy = 0;
+	}
 
-
-  PS2Mouse(DBus<MessagePS2> &bus_ps2, unsigned ps2port, unsigned hostmouse) : _bus_ps2(bus_ps2), _ps2port(ps2port), _hostmouse(hostmouse)
-  {
-    set_defaults();
-  }
+	PS2Mouse(DBus<MessagePS2> &bus_ps2, unsigned ps2port, unsigned hostmouse)
+	: _bus_ps2(bus_ps2), _ps2port(ps2port), _hostmouse(hostmouse)
+	{
+		set_defaults();
+	}
 };
 
 PARAM_HANDLER(mouse,
 	      "mouse:ps2port,hostmouse:  attach a PS2 mouse at the given PS2 port that gets input from the given hostmouse.",
 	      "Example: 'mouse:1,0x17'")
 {
-  PS2Mouse *dev = new PS2Mouse(mb.bus_ps2, unsigned(argv[0]), unsigned(argv[1]));
-  mb.bus_ps2.add(dev,   PS2Mouse::receive_static<MessagePS2>);
-  mb.bus_input.add(dev, PS2Mouse::receive_static<MessageInput>);
+	PS2Mouse *dev = new PS2Mouse(mb.bus_ps2, unsigned(argv[0]), unsigned(argv[1]));
+
+	mb.bus_ps2.add(dev,   PS2Mouse::receive_static<MessagePS2>);
+	mb.bus_input.add(dev, PS2Mouse::receive_static<MessageInput>);
 }
 
